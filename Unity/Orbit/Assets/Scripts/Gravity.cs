3c32aa706d7d8fdd066e875a00c4e5d6c8db0763
dennis-jung <dmjung@live.de>
2019-01-25 23:20:05 +0100

    scipts added

---
+++ Unity/Orbit/Assets/Scripts/Gravity.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Gravity : MonoBehaviour
+{
+
+    //public float maxGravDist = 4.0f;
+    public float maxGravity = 2.0f;
+
+    public GameObject nearestPlanet;
+
+    void Start()
+    {
+        //planets = GameObject.FindGameObjectsWithTag("Planet");
+    }
+
+    void FixedUpdate()
+    {
+
+        float dist = Vector3.Distance(nearestPlanet.transform.position, transform.position);
+
+        Vector3 v = nearestPlanet.transform.position - transform.position;
+        GetComponent<Rigidbody2D>().AddForce(v.normalized * (1.0f / dist ) * maxGravity);
+
+    }
+}
---
+++ Unity/Orbit/Assets/Scripts/Orbit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    public float multiplier = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(Vector3.forward, Time.deltaTime * multiplier);
+    }
+}
