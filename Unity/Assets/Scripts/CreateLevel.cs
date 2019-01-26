21561d553f09defb218ce015cf33358a52306d15
dennis-jung <dmjung@live.de>
2019-01-26 18:15:22 +0100

    playable distances
    playable distances in level creation

--- Unity/Assets/Scripts/CreateLevel.cs
+++ Unity/Assets/Scripts/CreateLevel.cs
@@ -17,6 +17,9 @@ public class CreateLevel : MonoBehaviour
     [SerializeField]
     private int countLevel3 = 20;
 
+    [SerializeField]
+    private float minDistance = 100.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,24 +27,50 @@ public class CreateLevel : MonoBehaviour
         {
             var vec = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0f);
             vec.Normalize();
-            Instantiate(planet, vec * 200.0f, Quaternion.identity);
+            var newPos = vec * 200.0f;
+            if (CheckDistances(newPos))
+            {
+                Instantiate(planet, newPos, Quaternion.identity);
+            }
         }
 
         for (int i = 0; i < countLevel2; i++)
         {
             var vec = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0f);
             vec.Normalize();
-            Instantiate(planet, vec * 400.0f, Quaternion.identity);
+            var newPos = vec * 400.0f;
+            if (CheckDistances(newPos))
+            {
+                Instantiate(planet, newPos, Quaternion.identity);
+            }
         }
 
         for (int i = 0; i < countLevel3; i++)
         {
             var vec = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0f);
             vec.Normalize();
-            Instantiate(planet, vec * 600.0f, Quaternion.identity);
+            var newPos = vec * 600.0f;
+            if (CheckDistances(newPos))
+            {
+                Instantiate(planet, newPos, Quaternion.identity);
+            }
         }
     }
 
+    private bool CheckDistances (Vector3 newPos)
+    {
+        var planets = GameObject.FindGameObjectsWithTag("PlanetNode");
+
+        foreach (var planet in planets)
+        {
+            if (Vector3.Distance(newPos, planet.transform.position) < minDistance )
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 
 }
