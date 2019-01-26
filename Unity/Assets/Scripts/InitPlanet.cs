b5be2946160fb8b117cf7d1f5f405850d933e4c1
dennis-jung <dmjung@live.de>
2019-01-26 23:36:24 +0100

    planets

---
+++ Unity/Assets/Scripts/InitPlanet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitPlanet : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] plants;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var plant in plants)
+        {
+            for (int i = 0; i < Random.Range(0,4); i++)
+            {
+                var vec = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0.0f);
+                vec.Normalize();
+                var newPos = vec * Random.Range(9.0f, 10.0f);
+                GameObject.Instantiate(plant, newPos + transform.position, Quaternion.FromToRotation(Vector3.up, newPos));
+            }
+            
+        }
+    }
+
+}
