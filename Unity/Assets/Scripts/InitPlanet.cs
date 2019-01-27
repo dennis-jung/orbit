b0f4f72301708d9c3d19be672a76d657d5fe1bc3
dennis-jung <dmjung@live.de>
2019-01-27 10:05:33 +0100

    changed pivots of gameobject

--- Unity/Assets/Scripts/InitPlanet.cs
+++ Unity/Assets/Scripts/InitPlanet.cs
@@ -5,20 +5,35 @@ using UnityEngine;
 public class InitPlanet : MonoBehaviour
 {
     [SerializeField]
-    GameObject[] plants;
+    GameObject[] outerPlants;
+
+    [SerializeField]
+    GameObject[] innerPlants;
     // Start is called before the first frame update
     void Start()
     {
-        foreach (var plant in plants)
+        foreach (var plant in outerPlants)
         {
             for (int i = 0; i < Random.Range(0,4); i++)
             {
-                var vec = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0.0f);
+                var vec = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-1.0f, 1.0f));
                 vec.Normalize();
-                var newPos = vec * Random.Range(9.0f, 10.0f);
+                var newPos = vec * 10.0f; //Random.Range(9.8f, 10.0f);
                 GameObject.Instantiate(plant, newPos + transform.position, Quaternion.FromToRotation(Vector3.up, newPos));
             }
             
+        }
+
+        foreach (var plant in innerPlants)
+        {
+            for (int i = 0; i < Random.Range(0, 4); i++)
+            {
+                var vec = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0.0f);
+                vec.Normalize();
+                var newPos = vec * Random.Range(1.0f, 7.0f);
+                GameObject.Instantiate(plant, newPos + transform.position, Quaternion.FromToRotation(Vector3.up, newPos));
+            }
+
         }
     }
 
