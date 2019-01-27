4fb64b62ef76d56a9b9457c7e53667c282af7502
dennis-jung <dmjung@live.de>
2019-01-27 11:56:47 +0100

    LevelManager updated

--- Unity/Assets/Scripts/CreateLevel.cs
+++ Unity/Assets/Scripts/CreateLevel.cs
@@ -1,12 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class CreateLevel : MonoBehaviour
 {
 
     [SerializeField]
-    private Transform planet;
+    private Transform[] planets;
 
     [SerializeField]
     private int countLevel1 = 5;
@@ -20,39 +21,46 @@ public class CreateLevel : MonoBehaviour
     [SerializeField]
     private float minDistance = 100.0f;
 
+    private int count = 0;
+
+    private Transform GetNextPlanet()
+    {
+        var nextCount = ++count % planets.Length;
+        return planets[nextCount];
+    }
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < countLevel1; i++)
         {
-            var vec = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0f);
+            var vec = new Vector3(UnityEngine.Random.Range(-10.0f, 10.0f), UnityEngine.Random.Range(-10.0f, 10.0f), 0f);
             vec.Normalize();
             var newPos = vec * 200.0f;
             if (CheckDistances(newPos))
             {
-                Instantiate(planet, newPos, Quaternion.identity);
+                Instantiate(GetNextPlanet(), newPos, Quaternion.identity);
             }
         }
 
         for (int i = 0; i < countLevel2; i++)
         {
-            var vec = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0f);
+            var vec = new Vector3(UnityEngine.Random.Range(-10.0f, 10.0f), UnityEngine.Random.Range(-10.0f, 10.0f), 0f);
             vec.Normalize();
             var newPos = vec * 400.0f;
             if (CheckDistances(newPos))
             {
-                Instantiate(planet, newPos, Quaternion.identity);
+                Instantiate(GetNextPlanet(), newPos, Quaternion.identity);
             }
         }
 
         for (int i = 0; i < countLevel3; i++)
         {
-            var vec = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0f);
+            var vec = new Vector3(UnityEngine.Random.Range(-10.0f, 10.0f), UnityEngine.Random.Range(-10.0f, 10.0f), 0f);
             vec.Normalize();
             var newPos = vec * 600.0f;
             if (CheckDistances(newPos))
             {
-                Instantiate(planet, newPos, Quaternion.identity);
+                Instantiate(GetNextPlanet(), newPos, Quaternion.identity);
             }
         }
     }
